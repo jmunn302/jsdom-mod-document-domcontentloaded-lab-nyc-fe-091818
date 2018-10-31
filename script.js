60192ecd9d278cfb6133ae31b9d688ae8238ca6e
Julie Munn <jmunn302@gmail.com>
2018-10-31 00:56:48 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 
-})
 
-document.querySelector("#hidden-div") {
+
+document.querySelector("div#hidden-div") {
   innerText = '301';
 }
+
+})
