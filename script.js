3f04d85b50af625e45d5ffd1d495e3b0d29a3bbf
Julie Munn <jmunn302@gmail.com>
2018-10-31 01:05:58 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!")); 
+
+const hidden = document.querySelector("#hidden-div") 
+
+ hidden.innerText = "301";
+
+
+})
